23d83865693781b75d5fd916714c210756467bd8
Willian Ohara <wyohara.dev@gmail.com>
2020-10-12 22:49:28 -0300

    dia 1 do nlw #3 adicionando o tilelayer do mapbox

--- web/src/screens/orphanagesMap.tsx
+++ web/src/screens/orphanagesMap.tsx
@@ -29,14 +29,17 @@ function OrphanagesMap() {
       center={[-22.7466566,-43.6898476]}
       zoom={15}
       style={{width:'100%', height:'100%'}}>
-        <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png '/>
+      /*<TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png '/>*/
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_API}`}
+        />
       </Map>
-
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFFFFF"/>
       </Link>
     </div>
   );
 }
+// <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png '/>
 
 export default OrphanagesMap;
